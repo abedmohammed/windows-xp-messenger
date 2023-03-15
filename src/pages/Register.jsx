f2bfbca699b3c0cbd575013b3cff41cccfc5e646
abedm <abedmohammed353@gmail.com>
2023-03-15 07:08:50 -0400

    Added restrictions to register and updated design

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -16,11 +16,16 @@ const Register = () => {
   const { error, setError, loading, setLoading } = useContext(ErrorContext);
   const [fileName, setFileName] = useState();
   const [passwordInput, setPasswordInput] = useState("");
+  const [passwordConfirmInput, setPasswordConfirmInput] = useState("");
   const { performQuery } = useQuerydb();
   const navigate = useNavigate();
 
   const handlePasswordInput = (e) => {
     setPasswordInput(e.target.value.replaceAll(" ", ""));
+  };
+
+  const handlePasswordConfirmInput = (e) => {
+    setPasswordConfirmInput(e.target.value.replaceAll(" ", ""));
   };
 
   const handleChooseImage = (e) => {
@@ -32,11 +37,14 @@ const Register = () => {
     const displayName = e.target[0].value.trim();
     const email = e.target[1].value.trim();
     const password = e.target[2].value;
-    const file = e.target[3].files[0];
+    const passwordConfirm = e.target[3].value;
+    const file = e.target[4].files[0];
 
     try {
       setLoading({ value: 10, message: "validating user details" });
       if (!displayName) throw new Error("Username cannot be empty.");
+      if (displayName.length > 16)
+        throw new Error("Username cannot be longer than 16 characters.");
 
       await performQuery({
         dbCollection: "userNames",
@@ -48,16 +56,18 @@ const Register = () => {
         },
       });
 
-      if (!email) throw new Error("Email cannot be empty");
+      if (!email) throw new Error("Email cannot be empty.");
 
-      if (!password) throw new Error("Password cannot be empty");
+      if (!password) throw new Error("Password cannot be empty.");
       if (password.length < 6)
         throw new Error(
           "Password must be longer than 6 characters, no spaces allowed."
         );
-      if (!file) throw new Error("Please use an image");
+      if (password !== passwordConfirm)
+        throw new Error("Passwords do not match.");
+      if (!file) throw new Error("Please use an image.");
       if (file.size / 1024 / 1024 > 1) {
-        throw new Error("Image size is too big! Maximum: 1MB");
+        throw new Error("Image size is too big! Maximum: 1MB.");
       }
 
       setLoading({ value: 20, message: "authenticating account" });
@@ -122,20 +132,52 @@ const Register = () => {
           <div className="register window-body">
             <h2 className="register__title">Register An Account:</h2>
 
-            <form className="field-row-stacked" onSubmit={handleSubmit}>
-              <input id="name" type="text" placeholder="username" />
+            <form className="register__form" onSubmit={handleSubmit}>
+              <div className="register__field register__field--username">
+                <label htmlFor="name">Username</label>
+                <input id="name" type="text" placeholder="username" />
+              </div>
 
-              <input type="email" placeholder="email" />
+              <div className="register__field register__field--email">
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="email" />
+              </div>
 
-              <input
-                onChange={handlePasswordInput}
-                value={passwordInput}
-                id="password"
-                type="password"
-                placeholder="password"
-              />
+              <div className="register__field register__field--password">
+                <label htmlFor="password">Password</label>
+                <input
+                  onChange={handlePasswordInput}
+                  value={passwordInput}
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                />
+              </div>
 
-              <div className="register__profile-pic">
+              <div className="register__field register__field--password-confirm">
+                <label htmlFor="passwordConfirm">Confirm Password</label>
+                <input
+                  onChange={handlePasswordConfirmInput}
+                  value={passwordConfirmInput}
+                  id="passwordConfirm"
+                  type="password"
+                  placeholder="confirm password"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="file"
+                  className="register__field register__field--image"
+                >
+                  <span>Add profile picture</span>
+                  <div className="register__image">
+                    <img src={Add} alt="" />
+                    <div className="register__attached">
+                      {fileName ? fileName : "None Selected"}
+                    </div>
+                  </div>
+                </label>
                 <input
                   style={{ display: "none", width: "max-content" }}
                   type="file"
@@ -143,19 +185,10 @@ const Register = () => {
                   onChange={handleChooseImage}
                   accept="image/*, image/*, image/*"
                 />
-                <label
-                  className="register__upload-image field-row"
-                  htmlFor="file"
-                >
-                  <img src={Add} alt="" />
-                  <span>Add profile picture</span>
-                </label>
-                {fileName && (
-                  <div className="register__attached">{fileName}</div>
-                )}
               </div>
               <button className="register__submit">Sign Up</button>
             </form>
+            <button className="register__submit">Sign In With Google</button>
             <p className="register__existing">
               You already have an account? <Link to="/login">Login</Link>
             </p>
